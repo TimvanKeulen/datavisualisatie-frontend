e499529e5e907197839b86d6d8ad41bdfa808f17
Tim van Keulen <timkeulen@gmail.com>
2018-03-19 13:14:20 +0100

    added electron to the project

---
+++ main.js
@@ -0,0 +1,19 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+
+// Listen for the app to be ready.
+app.on('ready', function(){
+	//create new window
+	mainWindow = new BrowserWindow({});
+	// load the html file into the window
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'mainWindow.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+});
