82772904c4e6d576f9e0d94f36eac120ede375bc
Maikel Veen <veenmaikel@gmail.com>
2018-03-29 00:31:41 +0200

    Add styling

--- main.js
+++ main.js
@@ -2,18 +2,27 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const {
+	app,
+	BrowserWindow
+} = electron;
 
 let mainWindow;
 
 // Listen for the app to be ready.
-app.on('ready', function(){
+app.on('ready', function () {
 	//create new window
-	mainWindow = new BrowserWindow({});
+	mainWindow = new BrowserWindow({
+		frame: false,
+		width: 1400,
+		height: 850
+	});
 	// load the html file into the window
 	mainWindow.loadURL(url.format({
 		pathname: path.join(__dirname, 'mainWindow.html'),
 		protocol: 'file:',
 		slashes: true
 	}));
+
+	
 });
