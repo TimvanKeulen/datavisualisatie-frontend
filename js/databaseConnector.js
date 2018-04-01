34317344110ca731b7e922280edec623537863d4
Bart Simons <bart@bartsimons.me>
2018-04-01 17:34:12 +0200

    Implement weather graphing

--- js/databaseConnector.js
+++ js/databaseConnector.js
@@ -4,37 +4,26 @@ class databaseConnector {
         var db = mongojs('hogeschool');
 
         this.trafficDocs = null;
-        this.trafficDocsScaled = null;
-        
         this.weatherDailyDocs = null;
-        this.weatherDailyDocsScaled = null;
-
         this.weatherMonthlyDocs = null;
-        this.weatherMonthlyDocsScaled = null;
-
         this.populationDocs = null;
-        this.populationDocsScaled = null;
 
         var that = this;
 
         db['traffic'].find(function(err, docs) {
             that.trafficDocs = docs;
-            that.trafficDocsScaled = docs;
         });
 
         db['weather-day'].find(function(err, docs) {
             that.weatherDailyDocs = docs;
-            that.weatherDailyDocsScaled = docs;
         });
 
         db['weather-month'].find(function(err, docs) {
             that.weatherMonthlyDocs = docs;
-            that.weatherMonthlyDocsScaled = docs;
         });
 
         db['population'].find(function(err, docs) {
             that.populationDocs = docs;
-            that.populationDocsScaled = docs;
         });
     }
 }
